5f40b444a63bbbae1ac23c283b721be29d58683b
Ankur2110 <ankurkaushikzinnov@gmail.com>
2023-08-14 20:16:20 +0530

    Range included

--- bmi.js
+++ bmi.js
@@ -11,4 +11,45 @@ heightinMeters = height/100
 	var BMI = (weight)/(heightinMeters* heightinMeters)
 document.getElementById("result").innerHTML = "Your  BMI is " + BMI.toFixed(2)
 
+BMI = parseFloat(BMI)
+
+console.log(BMI)
+
+if (BMI< 18.5) {
+  document.getElementById("result2").style.color = "red"
+	document.getElementById("result2").innerHTML = "You are underweight!"
+}
+
+if (BMI>18.5 && BMI<24.9) {
+	  document.getElementById("result2").style.color = "green"
+	document.getElementById("result2").innerHTML = "You have normal weight :)"
+}
+
+if (BMI>25 && BMI<29.9) {
+	  document.getElementById("result2").style.color = "red"
+	document.getElementById("result2").innerHTML = "You are overweight"
+
+}
+
+if (BMI>30  && BMI<34.9) {
+	  document.getElementById("result2").style.color = "red"
+	document.getElementById("result2").innerHTML = "You are in obesity class I!"
+
+}
+
+if (BMI>35 && BMI<39) {
+  document.getElementById("result2").style.color = "red"
+	document.getElementById("result2").innerHTML = "You are in obesity class II!!"
+
+
+}
+
+
+if (BMI>40) {
+	  document.getElementById("result2").style.color = "red"
+	document.getElementById("result2").innerHTML = "You are in obesity class III!!"
+
+}
+
+
 }
