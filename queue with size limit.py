37df1dbe9e77090ee8653a5e3ecb0f8fe7fb4730
Ankur <ankurkaushik03@gmail.com>
2024-03-04 09:49:55 +0530

    queue with size limit

--- queue with size limit.py
+++ queue with size limit.py
@@ -1,70 +1,65 @@
-#   Created by Elshad Karimov on 29/05/2020.
-#   Copyright © 2020 AppMillers. All rights reserved.
-
-class Queue:
-    def __init__(self, maxSize):
+class queue:
+    
+    def __init__(self,maxSize):
         self.items = maxSize * [None]
         self.maxSize = maxSize
         self.start = -1
-        self.top = -1 
-    
-    def __str__(self):
-        values = [str(x) for x in self.items]
-        return ' '.join(values)
-    
-    def isFull(self):
-        if self.top + 1 == self.start:
+        self.top = -1
+
+    def isfull(self):
+        if self.start ==0 and self.top +1 == self.maxSize:
             return True
-        elif self.start == 0 and self.top + 1 == self.maxSize:
+        elif self.top +1 == self.start:
             return True
-        else:
+        else: 
             return False
-    
-    def isEmpty(self):
+        
+    def isempty(self):
         if self.top == -1:
             return True
         else:
             return False
-    
+        
     def enqueue(self, value):
-        if self.isFull():
-            return "The queue is full"
-        else:
-            if self.top + 1 == self.maxSize:
+        if self.isfull():
+            return "The queue is already full"
+        else: 
+            if self.top +1 == self.maxSize:
                 self.top = 0
             else:
-                self.top += 1
-                if self.start == -1:
+                self.top +=1
+                if self.start ==-1:
                     self.start = 0
-            self.items[self.top] = value
-            return "The element is inserted at the end of Queue"
-    
+        self.items[self.top] = value 
+
     def dequeue(self):
-        if self.isEmpty():
-            return "There is not any element in the Queue"
+        if self.isempty():
+            return "The queue is already empty"
         else:
-            firstElement = self.items[self.start]
+            firstelement = self.items[self.start]
             start = self.start
             if self.start == self.top:
                 self.start = -1
                 self.top = -1
-            elif self.start + 1 == self.maxSize:
+            elif self.start +1 == self.maxSize:
                 self.start = 0
-            else:
-                self.start += 1
+            else: 
+                self.start +=1
             self.items[start] = None
-            return firstElement
+            return firstelement
     
     def peek(self):
-        if self.isEmpty():
-            return "There is not any element in the Queue"
+        if self.isempty():
+            return "Queue is empty"
         else:
             return self.items[self.start]
-    
+        
     def delete(self):
-        self.items = self.maxSize * [None]
-        self.top = -1
+        self.items = self.maxSize*[None]
         self.start = -1
+        self.top = -1
+
+        
 
 
 
